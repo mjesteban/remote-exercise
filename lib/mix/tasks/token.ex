bca0a07b3b76da27a7c1ba508a2bf5d8a8dd3961
mjesteban <marjoes13@gmail.com>
2023-05-13 03:50:28 +0800

    Auth token plug: Add doc

--- lib/mix/tasks/token.ex
+++ lib/mix/tasks/token.ex
@@ -1,10 +1,19 @@
 defmodule Mix.Tasks.Token do
+  @moduledoc """
+  Generates a token using Phoenix.Token and can be utilized as a Bearer token in the request header.
+  """
 
+  @doc """
+  Prints the Bearer token in a contrasting background.
+  """
   def print() do
     token = get_value()
     IO.puts IO.ANSI.format([:inverse, :bright, :underline, token])
   end
 
+  @doc """
+  Generates the Bearer token via Phoenix.Token
+  """
   def get_value() do
     Phoenix.Token.sign(
       Application.get_env(:be_exercise, ExerciseWeb.Endpoint)[:secret_key_base],
