771ac2034bd45be73b40f5b4952f72aa1715ea22
mjesteban <marjoes13@gmail.com>
2023-05-15 10:34:01 +0800

    Trim plug: Fix bug

--- lib/be_exercise_web/plugs/trim_request.ex
+++ lib/be_exercise_web/plugs/trim_request.ex
@@ -1,17 +1,20 @@
 defmodule ExerciseWeb.TrimRequest do
   def init(opts), do: opts
 
-  def call(%Plug.Conn{params: params_map} = conn, _opts) do
-
-    #todo: only do trimming on POST, PUT, and PATCH methods
-
+  def call(%Plug.Conn{params: params_map, method: method} = conn, _opts)
+    when method in ["POST", "PUT", "PATCH"]
+  do
     #get they key
     [{key, _}] = Map.to_list(params_map)
 
     trim_values = Enum.map(
       params_map[key],
-      fn {key, value} ->
-        {key, String.trim(value)}
+      fn {key, value} -> {key,
+        case is_binary(value) do
+          true -> String.trim(value)
+          _ -> value
+        end
+      }
     end)
     |> Enum.into(%{})
     trimmed_map = Map.put params_map, key, trim_values
